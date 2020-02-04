92da1cfdb2111c96afdf56cf6b4ea4a7b9d239e8
Jonathan Hiben <jonathan.hiben@ainos.lu>
2020-02-04 23:44:46 +0100

    linkedin pro

--- BrowserExtension/linkedin.js
+++ BrowserExtension/linkedin.js
@@ -1,16 +1,32 @@
-const _PROFILE_MATCH =
-  '.pv-top-card-v3--list > li.t-24,.pv-top-card--list > li.t-24,.profile-info > h1.searchable';
-const _SEARCH_RESULT_MATCH = '.name.actor-name,.search-result-profile-link';
+const _PROFILE_MATCH = [
+  '.pv-top-card-v3--list > li.t-24',
+  '.pv-top-card--list > li.t-24',
+  '.profile-info > h1.searchable',
+].join(',');
+
+const _SEARCH_RESULT_MATCH = [
+  '.name.actor-name',
+  '.search-result-profile-link',
+].join(',');
+
 const _SEARCH_POPUP_RESULT_MATCH =
   '.basic-typeahead__triggered-content.search-global-typeahead__content.search-box_focus .typeahead-suggestion .search-typeahead-v2__hit-info > span';
+
 const _SEARCH_RECENT_MATCH =
   '.search-typeahead-v2__history-list-carousel > li .typeahead-suggestion--carousel-item-text';
+
+const _PRO = [
+  '.lockup__content-title .artdeco-entity-lockup__title > a',
+  '.lockup__content-title div.artdeco-entity-lockup__title',
+  '.system-search-results__result-row .artdeco-entity-lockup__title > span',
+].join(',');
 
 const _ALL = [
   _PROFILE_MATCH,
   _SEARCH_RESULT_MATCH,
   _SEARCH_POPUP_RESULT_MATCH,
   _SEARCH_RECENT_MATCH,
+  _PRO,
 ].join(',');
 
 checker(_ALL);
