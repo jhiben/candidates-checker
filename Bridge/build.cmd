2905a5b05a4b856e67ca9c362009b8c1e6248f0d
Jonathan Hiben <jonathan.hiben@ainos.lu>
2020-04-05 15:56:56 +0200

    Update bridge build process

--- Bridge/build.cmd
+++ Bridge/build.cmd
@@ -1,1 +1,1 @@
-dotnet publish -c Release -o publish -r win10-x64 --self-contained true /p:PublishSingleFile=true /p:PublishTrimmed=true /p:PublishReadyToRun=true
+dotnet publish -c Release -o publish -r win10-x64 --self-contained true /p:PublishSingleFile=true
